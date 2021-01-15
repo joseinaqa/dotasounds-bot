723c19804c54d169e73cf3760f7e26bd90539157
Hossein Alimadad <hossein.alimadad@gmail.com>
2021-01-15 07:30:05 +0330

    :beetle: Use thread for scrape

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 import django
 django.setup()
 
+import threading
+
 from app.scrape import scrape
 from app.bot_server import start_bot
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-scrape()
+threading.Thread(target=scrape)
 start_bot()
