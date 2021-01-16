5d58bbd2b21c01a1a714f99ec69e6ab0689baba4
Hossein Alimadad <hossein.alimadad@gmail.com>
2021-01-16 04:44:57 +0330

    :hammer: Remove django

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py migrate
 web: python main.py
--- app/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Audio(models.Model):
-    title = models.CharField(max_length=256)
-    src = models.CharField(max_length=256)
