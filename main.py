425346e07db2e3e8ad4066f8cb111799a0d254e5
Hossein Alimadad <hossein.alimadad@gmail.com>
2021-01-16 04:41:02 +0330

    :beetle: Remove old code

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import logging
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-import django
-django.setup()
 
-import threading
-
-from app.scrape import scrape
 from app.bot_server import start_bot
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-threading.Thread(target=scrape).start()
 start_bot()
